037d01bb202a361f32a0098369460ffc958df3b0
AlexanderMaga <hmaganao2200@alumno.ipn.mx>
2025-06-10 15:50:46 -0600

    back

--- Ciberseguridad/06EjemploJWT/backend/routers/auth.js
+++ Ciberseguridad/06EjemploJWT/backend/routers/auth.js
@@ -20,10 +20,42 @@ router.post('/register', async (req, res) => {
                 res.status(200).send('Usuario registrado correctamente');
             }
         );
-    } catch (err) {
-        console.error('Error en el servidor:', err);
+    } catch (error) {
+        console.error('Error en el servidor:', error);
         res.status(500).send('Error del servidor');
     }
 });
+
+router.post('/login',(req, res)=>{
+    db.query('SELECT * FROM usuarios WHERE email = ?', [email], async(err, result)=>{
+        if(err){
+            console.log("Error en la consulta del login", err);
+            return res.status(500).send('Error en el servidor');
+        }
+        if(result.length === 0 ){
+            console.log("Usuario no encontrado", err);
+                return res.status(500).send('Usuario no encontrado');
+        }
+
+        const user = result[0];
+//validar pass hasheado
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid){
+            console.log("Contraseña incorrecta para usuario", email);
+            return res.status(401).send('Contraseña incorrecta user no autorizado')
+        }
+        const token = jwt.sign(
+            {id:user.id, email:user.email},
+            //proceso del hash
+            process.env.jwt_secret,
+            {expiresIn:'1h'}
+        );
+
+        console.log
+        res.json(token)
+        
+
+    })
+})
 
 module.exports = router;
